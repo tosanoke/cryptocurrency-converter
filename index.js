0bfb4e289ba1b19b259e9ed8dd514ec40bec8e62
tosanoke <tosanoke@gmail.com>
2019-12-31 14:36:45 +0100

    crptocurrency-converter
    A web app that converts crpyo currency to fiat.

---
+++ index.js
@@ -0,0 +1,51 @@
+ const express = require("express");
+const bodyParser = require("body-parser");
+const request = require('request');
+
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get ('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post ('/', function (req, res) {
+
+    let crypto = req.body.crypto;
+    let fiat = req.body.fiat;
+    let amount = req.body.amount;
+    let baseURL = 'https://apiv2.bitcoinaverage.com/convert/global';
+    let options = {
+        url: baseURL,
+        method: 'GET',
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+    };
+   
+   
+    request( options, function (error, response, body) {
+       
+
+      let data = JSON.parse(body);
+      let price = data.price;
+      let currentDate = data.time;
+
+      res.write('<p> the current date is ' + currentDate + '</p>');
+
+      res.write('<h1>'+ amount + crypto + ' is currently worth '+ price + fiat + '</h1>');
+
+      res.send();
+})});
+
+
+
+app.listen(3000, function() {
+    console.log('server is currently running on port 3000')
+});
+ 
